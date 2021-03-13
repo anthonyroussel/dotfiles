7fece5120e9893a4a98f2635e27cad86064f9c0e
Anthony Roussel <anthony.roussel17@gmail.com>
2021-03-13 21:41:12 +0100

    Install Kubernetes sources list and kubectl

--- run_once-01_install_sources_list.sh
+++ run_once-01_install_sources_list.sh
@@ -13,9 +13,16 @@ curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /etc/a
 echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc >/dev/null
 
+# Install kubectl APT sources
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-native-package-management
+#
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/kubernetes.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -fsSLo /etc/apt/trusted.gpg.d/kubernetes.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
 # Install packages
 sudo apt-get update
 sudo apt-get install --yes \
   jq \
+  kubectl \
   google-cloud-sdk \
   postgresql-client
