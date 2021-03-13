f80361ed2791a425d1d43727c0b5f422dfab5e0d
Anthony Roussel <anthony.roussel17@gmail.com>
2021-03-13 21:16:44 +0100

    Install google-cloud-sdk and jq

--- run_once-01_install_sources_list.sh
+++ run_once-01_install_sources_list.sh
@@ -6,3 +6,9 @@ set -e
 #
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/google-cloud-sdk.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /etc/apt/trusted.gpg.d/google-cloud-sdk.gpg >/dev/null
+
+# Install packages
+sudo apt-get update
+sudo apt-get install --yes \
+  jq \
+  google-cloud-sdk \
