614c7db3f6850396a48d7c9e01fd4391b90ddc87
Anthony Roussel <anthony.roussel17@gmail.com>
2021-03-13 21:34:07 +0100

    Install PostgreSQL client

--- run_once-01_install_sources_list.sh
+++ run_once-01_install_sources_list.sh
@@ -7,8 +7,15 @@ set -e
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/google-cloud-sdk.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 curl -sS https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /etc/apt/trusted.gpg.d/google-cloud-sdk.gpg >/dev/null
 
+# Install PostgreSQL APT sources
+# https://www.postgresql.org/download/linux/debian/
+#
+echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc >/dev/null
+
 # Install packages
 sudo apt-get update
 sudo apt-get install --yes \
   jq \
   google-cloud-sdk \
+  postgresql-client
