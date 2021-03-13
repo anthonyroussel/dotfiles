cf24f701ac6eae9253486b3d0484d52926d327b5
Anthony Roussel <anthony.roussel17@gmail.com>
2021-03-13 21:29:23 +0100

    Disable vim plugin update during chezmoi apply

--- run_01-install-vundle.sh
+++ run_01-install-vundle.sh
@@ -4,7 +4,7 @@ dir="$HOME/.vim/bundle/Vundle.vim"
 
 if cd $dir; then
   git pull
-  vim +PluginUpdate +qall
+  # vim +PluginUpdate +qall
 else
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
   vim +BundleInstall +qall
