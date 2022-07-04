3ec86340bc653166c9ef638bc4f351959a33b0e3
Anthony Roussel <anthony.roussel17@gmail.com>
2022-07-04 18:10:53 +0200

    Enable fontconfig, change keybindings for Switch windows

---
+++ home/dot_config/nixpkgs/desktop.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }:
+{
+  dconf.settings = {
+    "org/gnome/desktop/calendar" = {
+      show-weekdate = true;
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      clock-show-seconds = true;
+      clock-show-weekday = true;
+      gtk-theme = "Adwaita-dark";
+      show-battery-percentage = true;
+      text-scaling-factor = 1.10;
+    };
+    "org/gnome/desktop/sound" = {
+      events-sound = false;
+    };
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = "appmenu:minimize,maximize,close";
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      # Change Alt-Tab shortcut to switch windows.
+      switch-applications = ["<Super>Tab"];
+      switch-applications-backward = ["<Shift><Super>Tab"];
+      switch-windows = ["<Alt>Tab"];
+      switch-windows-backward = ["<Shift><Alt>Tab"];
+    };
+  };
+  fonts.fontconfig.enable = true;
+
+  home.packages = with pkgs; [
+    gnomeExtensions.applications-menu
+    gnomeExtensions.disconnect-wifi
+    gnomeExtensions.openweather
+    gnomeExtensions.places-status-indicator
+    gnomeExtensions.removable-drive-menu
+    gnomeExtensions.sound-output-device-chooser
+    gnomeExtensions.user-themes
+    gnomeExtensions.vitals
+    gnomeExtensions.workspace-indicator
+  ];
+}
