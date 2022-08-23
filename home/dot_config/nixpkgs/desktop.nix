e13ef446baf8007502454e0feed7e6e93115bbb8
Anthony Roussel <anthony.roussel17@gmail.com>
2022-08-23 13:37:09 +0200

    Add $HOME/.nix-profile/share to XDG_DATA_DIRS

--- home/dot_config/nixpkgs/desktop.nix
+++ home/dot_config/nixpkgs/desktop.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   dconf.settings = {
     "org/gnome/desktop/calendar" = {
@@ -62,5 +62,17 @@
     gnomeExtensions.user-themes
     gnomeExtensions.vitals
     gnomeExtensions.workspace-indicator
+  ];
+
+  # Add $HOME/.nix-profile/share to XDG_DATA_DIRS
+  xdg.systemDirs.data = [
+    # Nix profiles
+    "\${NIX_STATE_DIR:-/nix/var/nix}/profiles/default/share"
+    "${config.home.profileDirectory}/share"
+
+    # Distribution-specific
+    "/usr/local/share"
+    "/usr/share"
+    "/var/lib/snapd/desktop"
   ];
 }
